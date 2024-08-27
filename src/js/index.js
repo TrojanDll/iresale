beed1c39dcca42f1cf1540a478c34b965ce9f1b6
dzafarovt36@gmail.com <60787064+TrojanDll@users.noreply.github.com>
2024-08-27 15:19:50 +0300

    feat: faq section adaptation added

--- src/js/index.js
+++ src/js/index.js
@@ -43,3 +43,48 @@ document.querySelectorAll(".select").forEach((item) => {
 });
 
 // -----------------------------------------------------------------
+
+// ------------------------- Accordions ----------------------------
+
+document.querySelectorAll(".faq__accordion-item").forEach((el) => {
+  const summary = el.querySelector(".faq__accordion-header");
+  const content = el.querySelector(".faq__accordion-content");
+
+  summary.addEventListener("click", (e) => {
+    e.preventDefault();
+    el.classList.toggle("faq__accordion-content_opened");
+
+    if (el.open) {
+      slideUp(content, () => {
+        el.open = false;
+      });
+    } else {
+      el.open = true;
+      slideDown(content);
+    }
+  });
+});
+
+function slideUp(element, callback) {
+  const height = element.offsetHeight;
+  element.style.height = height + "px";
+  element.offsetHeight; // Force reflow
+  element.style.height = "0";
+  element.addEventListener("transitionend", function handler() {
+    element.removeEventListener("transitionend", handler);
+    callback();
+  });
+}
+
+function slideDown(element) {
+  element.style.height = "0";
+  element.offsetHeight; // Force reflow
+  const height = element.scrollHeight;
+  element.style.height = height + "px";
+  element.addEventListener("transitionend", function handler() {
+    element.removeEventListener("transitionend", handler);
+    element.style.height = "auto";
+  });
+}
+
+// --------------------------------------------------------------------
